a3f4b39376b91dae5a700f7bdfbf67d00e49079c
ana <ana.ana.in>
2021-11-30 23:13:20 +0530

    deployment fixes

--- src/app/pages/view-jobs/view-jobs.component.ts
+++ src/app/pages/view-jobs/view-jobs.component.ts
@@ -2,32 +2,32 @@ import { Component, OnInit } from "@angular/core";
 import { JobsService } from "src/app/services/jobs.service";
 
 @Component({
-  selector: "app-view-jobs",
-  templateUrl: "./view-jobs.component.html",
-  styleUrls: ["./view-jobs.component.scss"],
+	selector: "app-view-jobs",
+	templateUrl: "./view-jobs.component.html",
+	styleUrls: ["./view-jobs.component.scss"],
 })
 export class ViewJobsComponent implements OnInit {
-  applicationModal: boolean = false;
-  jobs = [];
-  jobId = "";
+	applicationModal: boolean = false;
+	jobs = [];
+	jobId = "";
 
-  constructor(private jobService: JobsService) {}
+	constructor(private jobService: JobsService) {}
 
-  ngOnInit(): void {
-    this.jobService.getAllJobs().subscribe((res) => {
-      //   if (res && res.data) {
-      //     this.jobs = res.data;
-      //   }
-    });
-  }
+	ngOnInit(): void {
+		this.jobService.getAllJobs().subscribe((res) => {
+			if (res && res.data) {
+				this.jobs = res.data;
+			}
+		});
+	}
 
-  viewApplications(job) {
-    this.applicationModal = true;
-    this.jobId = job.id;
-  }
+	viewApplications(job) {
+		this.applicationModal = true;
+		this.jobId = job.id;
+	}
 
-  closeApplications() {
-    this.applicationModal = false;
-    this.jobId = "";
-  }
+	closeApplications() {
+		this.applicationModal = false;
+		this.jobId = "";
+	}
 }
